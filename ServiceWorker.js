68a3ee1dc9d2225a28f45b6d9ddde4e29135dab7
ternary-system <3467486098@qq.com>
2025-06-17 13:46:07 +0800

    update

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,10 +1,10 @@
 const cacheName = "DefaultCompany-Kitchen-0.1.0";
 const contentToCache = [
-    "Build/build.loader.js",
-    "Build/build.framework.js",
-    "Build/build.data",
-    "Build/build.wasm",
-    "TemplateData/style.css"
+    "./Build/build.loader.js",
+    "./Build/build.framework.js",
+    "./Build/build.data",
+    "./Build/build.wasm",
+    "./TemplateData/style.css"
 
 ];
 
